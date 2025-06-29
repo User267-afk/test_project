9b2a50d71d543c7c7c42162a92d188c209e2525f
crystal <mhrzncrystal@gmail.com>
2025-06-29 20:13:19 +0545

    add a3

---
+++ a3.cpp
@@ -0,0 +1,19 @@
+//3. Write a program to get input a string and print the string and its reverse. Define your own function to reverse the string in your program. 
+#include <iostream>
+using namespace std;
+
+void 
+
+int main()
+{
+  a[10], b[10], i, j;
+  cout<< "Enter any string" << endl;
+  for (i=0; i<10 ; i++)
+  {
+    cin >> a[i];
+  }
+for()
+
+
+return 0;
+}
